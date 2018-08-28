d6bd82aba845180635772735463532aa39d2ac49
JessB1202 <jessicrr@gmail.com>
2018-08-27 21:35:19 -0400

    Second Commit
    Added validation code for user inputted dates and attempted to validate situation when end date is less than start date.

--- GCLab1Part2/Program.cs
+++ GCLab1Part2/Program.cs
@@ -7,31 +7,69 @@ namespace GCLab1Part2
         public static void Main(string[] args)
         {
             //assinging varibles
-            DateTime startDate, endDate; 
+            string inputStart, inputEnd;
+            DateTime startDate, endDate;
+            int diffDays, diffHours, diffMins;
 
-            //Asking the user for dates
-            Console.WriteLine("Please enter a start date, YYYY,MM,DD, please include the commas.");
-            startDate = Convert.ToDateTime(Console.ReadLine());
+            //Asking the user for start date
+            Console.WriteLine("Please enter a start date, MM,DD,YYYY. Please include the commas.");
+            inputStart = Console.ReadLine();
 
-            Console.WriteLine("Please enter an end date, YYYY,MM,DD, please include the commas.");
-            endDate = Convert.ToDateTime(Console.ReadLine());
+            // Validation of start date
+            while (!DateTime.TryParse(inputStart, out startDate))
+            {
+                Console.WriteLine("The start date you entered is invalid. Please input a valid start date.");
+                inputStart = Console.ReadLine();
+            }
 
-            //Confirmation of input
-            Console.WriteLine(startDate);
-            Console.WriteLine(endDate);
+            // Asking the user for end date
+            Console.WriteLine("Please enter an end date, MM,DD,YYYY. Please include the commas.");
+            inputEnd = Console.ReadLine();
+
+            //Validation of end date
+            while (!DateTime.TryParse(inputEnd, out endDate))
+            {
+                Console.WriteLine("The end date you entered is invalid. Please input a valid end date.");
+                inputEnd = Console.ReadLine();
+            }
+
+            //// Validation that end date is greater than start date
+            //while (endDate < startDate)
+            //{
+            //    Console.WriteLine("The end date you entered is earlier than your start date. Please provide a later end date.");
+            //    inputEnd = Console.ReadLine();
+            //}
+
+            ////Re-validate end date
+
+            //while (!DateTime.TryParse(inputEnd, out endDate))
+            //{
+            //    Console.WriteLine("The end date you entered is invalid. Please input a valid end date.");
+            //    inputEnd = Console.ReadLine();
+            //}
+
+            //Confirming with user the start and end dates
+            Console.WriteLine();
+            Console.WriteLine("The start date you entered is {0}.", startDate.Date);
+            Console.WriteLine("The end date you entered is {0}.", endDate.Date);
+            Console.WriteLine();
 
             //Calculating the difference in dates
             TimeSpan diffDate = endDate - startDate;
 
-            Console.WriteLine(diffDate);
-
             // Difference in days
-            Console.WriteLine(diffDate.Days);
+            diffDays = diffDate.Days;
+            Console.WriteLine("The number of days between {0} and {1} are {2} days.", startDate.Date, endDate.Date, diffDays);
+            Console.WriteLine();
 
             // Difference in hours
-            Console.WriteLine(diffDate.Hours + (diffDate.Days * 24));
+            diffHours = diffDate.Hours + (diffDays * 24);
+            Console.WriteLine("The number of hours between {0} and {1} are {2} hours.", startDate.Date, endDate.Date, diffHours);
+            Console.WriteLine();
 
-            Console.WriteLine(diffDate.Minutes);
+            // Difference in minutes
+            diffMins = diffDate.Minutes + (diffHours * 60);
+            Console.WriteLine("The number of minutes between {0} and {1} are {2} minutes.", startDate.Date, endDate.Date, diffMins);
         }
     }
 }
