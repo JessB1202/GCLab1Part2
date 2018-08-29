68dc70c3a73ae92aed07fd15848cc97ecedb5161
Jessica Billingslea <jessicrr@gmail.com>
2018-08-28 21:31:12 -0400

    Updated output to only show MM/dd/yyyy

--- GCLab1Part2/Program.cs
+++ GCLab1Part2/Program.cs
@@ -6,12 +6,12 @@ namespace GCLab1Part2
     {
         public static void Main(string[] args)
         {
-            //assinging varibles
+            // Assigning varibles
             string inputStart, inputEnd;
             DateTime startDate, endDate;
             int diffDays, diffHours, diffMins;
 
-            //Asking the user for start date
+            // Asking the user for start date
             Console.WriteLine("Please enter a start date, MM,DD,YYYY. Please include the commas.");
             inputStart = Console.ReadLine();
 
@@ -26,35 +26,35 @@ namespace GCLab1Part2
             Console.WriteLine("Please enter an end date, MM,DD,YYYY. Please include the commas.");
             inputEnd = Console.ReadLine();
 
-            //Validation of end date
+            // Validation of end date
             while (!DateTime.TryParse(inputEnd, out endDate))
             {
                 Console.WriteLine("The end date you entered is invalid. Please input a valid end date.");
                 inputEnd = Console.ReadLine();
             }
 
-            //Confirming with user the start and end dates
+            // Confirming with user the start and end dates
             Console.WriteLine();
-            Console.WriteLine("The start date you entered is {0}.", startDate.Date);
-            Console.WriteLine("The end date you entered is {0}.", endDate.Date);
+            Console.WriteLine("The start date you entered is {0:MM/dd/yyyy}.", startDate);
+            Console.WriteLine("The end date you entered is {0:MM/dd/yyyy}.", endDate);
             Console.WriteLine();
 
-            //Calculating the difference in dates
+            // Calculating the difference in dates
             TimeSpan diffDate = endDate - startDate;
 
             // Difference in days
             diffDays = diffDate.Days;
-            Console.WriteLine("The number of days between {0} and {1} are {2} days.", startDate.Date, endDate.Date, Math.Abs(diffDays));
+            Console.WriteLine("The number of days between {0:MM/dd/yyyy} and {1:MM/dd/yyyy} are {2} days.", startDate, endDate, Math.Abs(diffDays));
             Console.WriteLine();
 
             // Difference in hours
             diffHours = diffDate.Hours + (diffDays * 24);
-            Console.WriteLine("The number of hours between {0} and {1} are {2} hours.", startDate.Date, endDate.Date, Math.Abs(diffHours));
+            Console.WriteLine("The number of hours between {0:MM/dd/yyyy} and {1:MM/dd/yyyy} are {2} hours.", startDate, endDate, Math.Abs(diffHours));
             Console.WriteLine();
 
             // Difference in minutes
             diffMins = diffDate.Minutes + (diffHours * 60);
-            Console.WriteLine("The number of minutes between {0} and {1} are {2} minutes.", startDate.Date, endDate.Date, Math.Abs(diffMins));
+            Console.WriteLine("The number of minutes between {0:MM/dd/yyyy} and {1:MM/dd/yyyy} are {2} minutes.", startDate, endDate, Math.Abs(diffMins));
         }
     }
 }
